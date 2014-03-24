6925214ef7199764a4ed4176e21611fd9fd3e33e
webus <webusnix@gmail.com>
2014-03-24 08:31:42 +0400

    update python ident. spaces not tabs

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -17,4 +17,10 @@
 		(eq 1 (point-max)))
 	 (insert-file "~/.emacs.d/templates/template.py")))
 
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq indent-tabs-mode nil) ;;; (setq indent-tabs-mode t - for use tabs / nil - for use spaces
+            (setq tab-width 4)
+            (setq python-indent 4)))
+
 (provide 'init-python-mode)
