f82663399ffd70d66ae680fba0cd38022bf79819
webus <webusnix@gmail.com>
2014-08-28 15:51:35 +0400

    added jade

---
+++ lisp/init-jade.el
@@ -0,0 +1,7 @@
+(add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
+(require 'sws-mode)
+(require 'jade-mode)
+(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+(provide 'init-jade)
