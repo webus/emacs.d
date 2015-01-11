d4a69475e0b1ca9f74e9ab35cf05bd1fa93b611c
webus <webusnix@gmail.com>
2015-01-11 18:39:35 +0300

    merge with upstream

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -5,6 +5,8 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+(require-package 'pip-requirements)
+
 (add-hook 'python-mode-hook 'jedi:setup)
 (setq jedi:setup-keys t)                      ; optional
 (setq jedi:complete-on-dot t)                 ; optional
