71b8b04c30f99f866fa5ca83beccc545bb3a3647
webus <webusnix@gmail.com>
2016-06-22 22:48:03 +0300

    remove git-rebase-mode (part of magit now too)

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -2,7 +2,6 @@
 ;; TODO: smerge-mode
 (require-package 'magit)
 (require-package 'git-blame)
-(require-package 'git-rebase-mode)
 (require-package 'gitignore-mode)
 (require-package 'gitconfig-mode)
 (require-package 'git-messenger) ;; Though see also vc-annotate's "n" & "p" bindings
